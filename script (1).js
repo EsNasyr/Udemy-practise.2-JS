e1b83f62e2bf29ad210a812b30e1b3ab995ee81f
Eren-Yeag <esenbaevnasyr@gmail.com>
2022-07-12 14:20:44 +0300

    finished practise.2

--- script (1).js
+++ script (1).js
@@ -27,29 +27,31 @@ const personalMovieDB = {
 };
 
 for (let i = 0; i < 2; i++) {
-    const a = prompt('Один из последних просмотренных фильмов?', ''),
-        b = prompt('На сколько оцените его?', '');
-
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        const a = prompt('Один из последних просмотренных фильмов?');
+        const b = +prompt('На сколько оцените его?');
+    if (a != null && b != null && a != '' && b != '' && a.length <= 50 ) {
         personalMovieDB.movies[a] = b;
-        console.log('done');
-    } else {
-        console.log('error');
+        console.log('done')
+    }
+    else {
         i--;
     }
 }
+console.log(personalMovieDB);
 
-
-if (numberOfFilms <= 10) {
+if (numberOfFilms < 1) {
+    alert('Не просмотрено ни одного фильма')
+}
+if (numberOfFilms < 10) {
     alert('Просмотрено довольно мало фильмов')
 }
 else if (numberOfFilms >= 10 && numberOfFilms <= 30) {
     alert('Вы классический зритель')
 }
-else if (numberOfFilms >= 30) {
+else if (numberOfFilms > 30){
     alert('Вы киноман!')
 }
 else {
-    alert('Произошла ошибка!')
+    alert('Произлошла ошибка!')
 }
 // Код возьмите из предыдущего домашнего задания
